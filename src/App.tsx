360a8ba89f4cee4d69b251fe0913025e104483a7
CHERTORYLSKY Ilya <ilya.chertorylsky@raiffeisen.ru>
2020-09-30 11:30:53 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div`
+    font-size 24pt; 
+    color: red;
+`
+
+export default function App()
+{
+    return <Box>Hello, world!</Box>
+}
+
+
